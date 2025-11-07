ebb45be940b94b6992a98cf9e732f158a846d3e5
MB Samuel <maryboydsamuel@gmail.com>
2025-11-07 12:57:23 -0500

    one more fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://agentexperience.design',
   output: 'static',
-  adapter: netlify(),
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
