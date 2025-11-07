296b0cbbbda198ef82175e7b9199a96b82d5361b
MB Samuel <maryboydsamuel@gmail.com>
2025-11-07 14:41:48 -0500

    images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://agentexperience.design',
   output: 'static',
+  adapter: netlify({
+    imageCDN: false,
+  }),
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
